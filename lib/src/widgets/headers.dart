22ac43b366b91be5934dc760fef01eaa09c4e75f
JhonZuluaga007 <jhonsalazar93@hotmail.com>
2022-07-01 22:47:17 -0500

    a circular header is created

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -11,3 +11,19 @@ class SquareHeaders extends StatelessWidget {
     );
   }
 }
+
+class CircularHeaders extends StatelessWidget {
+  const CircularHeaders({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      decoration: const BoxDecoration(
+          color: Color(0xff615AAB),
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(70),
+              bottomRight: Radius.circular(30))),
+    );
+  }
+}
